fbb4aa1f5a330c9aa6e27da861762c47c31e94ba
Dzianis Antanouski <dzianis.antanouski@LeverX.local>
2022-08-14 10:53:51 +0300

    refactoring

--- src/controller/Test.controller.ts
+++ src/controller/Test.controller.ts
@@ -2,25 +2,21 @@ import BaseController from "./BaseController";
 import JSONModel from "sap/ui/model/json/JSONModel";
 import models from "../model/models";
 import ListBase from "sap/m/ListBase";
-import Context from 'sap/ui/model/Context';
+import Context from "sap/ui/model/Context";
 import MessageBox from "sap/m/MessageBox";
 import Dialog from "sap/m/Dialog";
 import Control from "sap/ui/core/Control";
 import Fragment from "sap/ui/core/Fragment";
 import UI5Element from "sap/ui/core/Element";
 
-
 /**
  * @namespace webapp.typescript.controller
  */
 export default class Start extends BaseController {
   oFragment: Promise<Dialog | Control | Control[]>;
-  public onInit(): void {
-    const qListModel: JSONModel = models.createQListModel();
-    this.setModel(qListModel);
-  }
+
   onSubmitPress(): void {
-    let checkedAnswers = this.getCheckedAnswers();
+    const checkedAnswers = this.getCheckedAnswers();
     if (this.checkBeforeSubmit(checkedAnswers)) {
       this.checkResultsOfTest();
     } else {
@@ -29,31 +25,32 @@ export default class Start extends BaseController {
   }
 
   getCheckedAnswers(): Array<Array<string>> {
-    let array: Array<Context[]> = this.getView().getControlsByFieldGroupId('table').filter(
-      (oControl) => oControl.getMetadata().getElementName() === "sap.m.Table").map((control) => (control as ListBase).getSelectedContexts())
-    return array.map(context => context.map(el => el.getPath()))
+    const array: Array<Context[]> = this.getView()
+      .getControlsByFieldGroupId("table")
+      .filter((oControl) => oControl.getMetadata().getElementName() === "sap.m.Table")
+      .map((control) => (control as ListBase).getSelectedContexts());
+    return array.map((context) => context.map((el) => el.getPath()));
   }
 
   checkBeforeSubmit(checkedAnswers: Array<Array<string>>) {
-    return checkedAnswers.every(array => array.length > 0)
+    return checkedAnswers.every((array) => array.length > 0);
   }
-  onCancelPress() {
+  onCancelPress(): void {
     MessageBox.confirm("Do you want to RESET all selected answers?", {
       onClose: (oAction: string) => {
-        console.log(oAction)
+        console.log(oAction);
         if (oAction === "OK") {
-          this.resetAllSelectedAnswers()
-
-          //   }
+          this.resetAllSelectedAnswers();
         }
-      }
-    })
+      },
+    });
   }
-  resetAllSelectedAnswers() {
-   
+  resetAllSelectedAnswers(): void {
+    // ?? in process
+    this.getModel();
   }
-  checkResultsOfTest(){
-    debugger;
+  checkResultsOfTest(): void {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
     if (!this.oFragment) {
       const oView = this.getView();
       this.oFragment = Fragment.load({
@@ -65,8 +62,8 @@ export default class Start extends BaseController {
         return oMessagePopover;
       });
     }
-    const oModel = this.getModel() as JSONModel;
+    // const oModel = this.getModel() as JSONModel;
     // oModel.setProperty("/newQuestion", JSON.parse(JSON.stringify(this.newQuestion)));
-    this.oFragment.then((oMessagePopover) => (oMessagePopover as Dialog).open());
+    void this.oFragment.then((oMessagePopover) => (oMessagePopover as Dialog).open());
   }
 }
