07faf431ee6724cf1953540c412bbb6e7e176289
Dzianis Antanouski <dzianis.antanouski@LeverX.local>
2022-08-23 11:54:00 +0300

    pre

--- src/controller/Auth.controller.ts
+++ src/controller/Auth.controller.ts
@@ -1,5 +1,6 @@
 import BaseController from "./BaseController";
 import JSONModel from 'sap/ui/model/json/JSONModel';
+import Auth from "../db/Auth";
 
 
 /**
@@ -20,26 +21,9 @@ export default class Start extends BaseController {
     oAuthModel.setProperty("/btnVisible", false)
     const email: string = oAuthModel.getProperty("/email") as string
     const password: string = oAuthModel.getProperty("/password") as string
-    void this.getToken(email, password)
+    void Auth.getToken(email, password)
       .then(void oAuthModel.setProperty("/btnVisible", true))
   }
 
-  public async getToken (email: string, password: string): Promise<string> {
-    interface IToken {
-      idToken: string
-    }   
-    const apiKey = `AIzaSyC-14jeZmSOdG1HPzl5bdZ_aLBBFVEg_-8`
-    return await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, {
-      method: "POST",
-      body: JSON.stringify({
-        email, password,
-        returnSecureToken: true
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(response => response.json() as Promise<string>)
-      .then(sToken => (sToken as unknown as IToken).idToken)
-  }
+  
 }
