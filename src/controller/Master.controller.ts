ff5d645e559c4c704db847329256aba4c6ad3269
Dzianis Antanouski <dzianis.antanouski@LeverX.local>
2022-09-11 02:17:59 +0300

    fix master

--- src/controller/Master.controller.ts
+++ src/controller/Master.controller.ts
@@ -2,7 +2,6 @@ import MessageToast from "sap/m/MessageToast";
 import EventBus from "sap/ui/core/EventBus";
 import BaseController from "./BaseController";
 import Event from "sap/ui/base/Event";
-import CRUDModel from '../model/CRUDModel';
 
 /**
  * @namespace webapp.typescript.controller
